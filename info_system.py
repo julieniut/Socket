9c12bdd9b642742173d9585fb720541f22e7a890
julieniut <92865222+julieniut@users.noreply.github.com>
2022-11-28 16:29:12 +0100

    ADD commandes serveur

---
+++ info_system.py
@@ -0,0 +1,15 @@
+import platform, psutil
+
+
+System = platform.uname()
+
+print(f"System: {System.system}")
+print(f"Node Name: {System.node}")
+print(f"Release: {System.release}")
+print(f"Version: {System.version}")
+print(f"Machine: {System.machine}")
+print(f"Processor: {System.processor}")
+
+print(f"Memory :{psutil.virtual_memory()}")
+print("cpu pourcent",psutil.cpu_percent(4))
+
